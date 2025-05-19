4bdbf6bfdd2750adbee8a35e55e07494567ad357
LaurentB85 <l.bernabe@optimumcit.com>
2025-05-19 10:20:58 +0200

    fix(angular) ajout interceptor pour récupérér le token et l'envoyer à chaque appel, ajout package à jour

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,14 @@ import {provideRouter} from '@angular/router';
 import {provideAnimations} from '@angular/platform-browser/animations';
 
 import {routes} from './app.routes';
+import {jwtInterceptor} from './common/interceptors/jwt.interceptor';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
+    provideHttpClient(withInterceptors([jwtInterceptor])),
     provideAnimations()
   ]
 };
