46ef9b720b24557d7b9fd1b694613f55595c2691
LaurentB85 <l.bernabe@optimumcit.com>
2025-09-18 14:12:54 +0200

    fix(angular)  correction apiUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  apiUrl: 'https://api.bernabe.codes',
   // apiUrl: 'https://api.bernabe.laurent',
-  apiUrl: 'http://159.89.20.85:3000',
+  // apiUrl: 'http://159.89.20.85:3000',
 };
